7ef75a8c0cef113152f60c5803d01b8e7f954891
Adrien Porter <adrien.porter@gmail.com>
2024-05-06 22:46:17 -0700

    add volatility calculations and refactor notebook

--- stocks_feed/risk.py
+++ stocks_feed/risk.py
@@ -1,13 +1,16 @@
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 from stocks_feed.dataloader import Stock, Index
 
 
 class Risk:
-    def __init__(self, stock, index):
+    def __init__(self, stock, index, nmv=10_000_000):
         self.stock = stock
         self.index = index
-        self.ols_results = self.ols()
+        self.ols_results, self.X, self.y = self.ols()
+        self.risk_params = self.get_risk_params()
+        self.single_stock_vols = self.get_single_stock_vol(nmv)
 
     def concat_series(self):
         stock_returns = self.stock.df["daily_return"]
@@ -23,7 +26,34 @@ class Risk:
         X = df[self.index.index_name]
         X = sm.add_constant(X)
         results = sm.OLS(y, X).fit()
-        return results
+        return results, X, y
 
     def ols_summary(self):
         print(self.ols_results.summary())
+
+    def get_risk_params(self):
+        alpha = self.ols_results.params["const"]
+        beta = self.ols_results.params[self.index.index_name]
+        daily_mkt_vol_perc = np.std(self.X[self.index.index_name]) * 100
+        daily_idio_vol_perc = (
+            np.std(self.y - (alpha + beta * self.X[self.index.index_name])) * 100
+        )
+        risk_params = {
+            "alpha": alpha,
+            "beta": beta,
+            "daily_mkt_vol_perc": daily_mkt_vol_perc,
+            "daily_idio_vol_perc": daily_idio_vol_perc,
+        }
+        return risk_params
+
+    def get_single_stock_vol(self, nmv=10_000_000):
+        market_vol = (
+            self.risk_params["beta"]
+            * self.risk_params["daily_mkt_vol_perc"]
+            * nmv
+            / 100
+        )
+        idio_vol = self.risk_params["daily_idio_vol_perc"] * nmv / 100
+        total_vol = np.sqrt(market_vol**2 + idio_vol**2)
+        vols = {"market_vol": market_vol, "idio_vol": idio_vol, "total_vol": total_vol}
+        return vols
